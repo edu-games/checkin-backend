23d8888b2f5ec83cb0f4be5faf5b62355e7ad297
Mike Landers <landers.mike@gmail.com>
2020-02-24 12:54:43 -0800

    get query works

--- db.js
+++ db.js
@@ -1,25 +1,43 @@
-const Sequelize = require("sequelize");
+// const Sequelize = require("sequelize");
 
-// const db = new Sequelize(process.env.DATABASE_URL);
-const db = new Sequelize({
-	username: process.env.DB_USERNAME,
+const { Client } = require("pg");
+const db = new Client({
+	user: process.env.DB_USERNAME,
+	host: process.env.DB_HOSTNAME,
 	database: process.env.DB_DATABASE,
 	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOSTNAME,
 	port: 5432,
-	ssl: true,
-	dialect: "postgres",
-	dialectOptions: {
-		ssl: { require: true }
+	ssl: true
+});
+
+db.connect(err => {
+	if (err) {
+		console.error("‼️  Unable to connect to the database:", err);
+	} else {
+		console.log("✅  Database connection has been established successfully.");
 	}
 });
 
-db.authenticate()
-	.then(() => {
-		console.log("✅  Database connection has been established successfully.");
-	})
-	.catch(err => {
-		console.error("‼️  Unable to connect to the database:", err);
-	});
+// const db = new Sequelize(process.env.DATABASE_URL);
+// const db = new Sequelize({
+// 	username: process.env.DB_USERNAME,
+// 	database: process.env.DB_DATABASE,
+// 	password: process.env.DB_PASSWORD,
+// 	host: process.env.DB_HOSTNAME,
+// 	port: 5432,
+// 	ssl: true,
+// 	dialect: "postgres",
+// 	dialectOptions: {
+// 		ssl: { require: true }
+// 	}
+// });
+
+// db.authenticate()
+// 	.then(() => {
+// 		console.log("✅  Database connection has been established successfully.");
+// 	})
+// 	.catch(err => {
+// 		console.error("‼️  Unable to connect to the database:", err);
+// 	});
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const db = require("./db");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Sanity Check!"));
+app.get("/students", async (req, res) => {
+	const { rows } = await db.query(`SELECT * FROM Students`);
+
+	res.status(200).json({ data: rows });
+});
 
 app.listen(port, () => console.log(`🚀  Server running on port ${port}!`));
