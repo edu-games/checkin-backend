3ae7af50cc925a678a04363fde1e607d3e86c2df
Mike Landers <landers.mike@gmail.com>
2020-02-24 10:16:38 -0800

    update to db url

--- db.js
+++ db.js
@@ -1,14 +1,5 @@
 const Sequelize = require("sequelize");
 
-// Option 1: Passing parameters separately
-const db = new Sequelize(
-	process.env.DB_DATABASE,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
-	{
-		host: process.env.DB_HOST,
-		dialect: "postgres"
-	}
-);
+const db = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = db;
